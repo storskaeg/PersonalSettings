24c8684f479e472022a9cbd8f243f5a15a667f95
liam <liam@storskegg.org>
2016-02-20 10:16:23 -0500

    Better distro identification

--- doit.sh
+++ doit.sh
@@ -7,3 +7,20 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Do something under Linux
   echo "Ok, Linux..."
 fi
+
+# Determine OS platform
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" == "linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+# For everything else (or if above failed), just use generic identifier
+[ "$DISTRO" == "" ] && export DISTRO=$UNAME
+echo $DISTRO
+unset UNAME
